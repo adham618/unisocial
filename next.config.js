472c2fe482b4bf9306c205674123948b2010f7e7
Adham Tarek <adhamtarek291@gmail.com>
2023-02-11 08:41:30 +0200

    feat: fix svgr

--- next.config.js
+++ next.config.js
@@ -15,19 +15,33 @@ const nextConfig = {
   // },
 
   // SVGR
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/i,
+  //     issuer: /\.[jt]sx?$/,
+  //     use: [
+  //       {
+  //         loader: '@svgr/webpack',
+  //         options: {
+  //           typescript: true,
+  //           icon: true,
+  //         },
+  //       },
+  //     ],
+  //   });
+
+  //   return config;
+  // },
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+        // for webpack 5 use
+        // { and: [/\.(js|ts)x?$/] }
+      },
+
+      use: ['@svgr/webpack'],
     });
 
     return config;
