f5e5020c6946a10b8195215ce03db215d782a89a
Adham Tarek <adhamtarek291@gmail.com>
2023-02-11 07:02:30 +0200

    feat: add header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import * as React from 'react';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/about', label: 'About' },
+  { href: '/features', label: 'Features' },
+  { href: '/contact', label: 'Contact' },
 ];
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        Home
+      <div className='layout flex items-center justify-between py-5'>
+        <Link href='/'>
+          <Image src='/svg/Logo.svg' width={161} height={42} alt='logo' />
+        </Link>
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>{label}</li>
+              <li key={`${href}${label}`}>
+                <Link className='font-normal hover:text-primary' href={href}>
+                  {label}
+                </Link>
+              </li>
             ))}
           </ul>
         </nav>
+        <div className='flex items-center space-x-4'>
+          <Link
+            href='/login'
+            className='text-base font-normal hover:opacity-70'
+          >
+            Login
+          </Link>
+          <Link
+            href='/signup'
+            className='rounded-full bg-primary px-6 py-3 text-base font-normal text-white transition-colors hover:bg-opacity-90'
+          >
+            Sign Up
+          </Link>
+        </div>
       </div>
     </header>
   );
