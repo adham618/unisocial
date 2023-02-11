f65f12413c4c384ff078066dbaa4443e1c654604
Adham Tarek <adhamtarek291@gmail.com>
2023-02-12 00:57:11 +0200

    feat:  fix padding

--- src/components/StudentSocialSection/index.tsx
+++ src/components/StudentSocialSection/index.tsx
@@ -8,7 +8,7 @@ import HightRigntVector from '@/assets/HighRight.svg';
 export default function index() {
   return (
     <section className='max-w-full overflow-hidden'>
-      <div className='layout grid grid-cols-12 gap-8 py-16 text-center'>
+      <div className='layout grid grid-cols-12 gap-8 pb-10 pt-16 text-center'>
         <div className='wow animate__animated animate__fadeInUp col-span-8 rounded-[22px] bg-primary px-12 py-10'>
           <h2 className='relative top-0 left-0 text-left text-[56px] font-bold italic leading-[64px] tracking-tight text-white'>
             <HightRigntVector className='absolute -top-3 -left-4 fill-white text-[23px]' />
@@ -42,7 +42,7 @@ export default function index() {
           <EllipseTop className='absolute  top-[148px] left-[-186px] -z-10 h-[299.61px] w-[435.88px]' />
         </div>
       </div>
-      <div className='layout grid grid-cols-3 gap-8'>
+      <div className='layout grid grid-cols-3 gap-8 pb-16'>
         <div
           className='wow animate__animated animate__fadeInUp rounded-[22px] bg-white px-8 py-10 text-left'
           style={{ boxShadow: '0px 13px 100px rgba(199, 199, 199, 0.25)' }}
